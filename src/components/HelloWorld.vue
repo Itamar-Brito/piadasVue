3a4ea63f7f57b33ecb5d1320ff713a5ddc7efbe8
Itamar-Brito <itamar.brito@gmail.com>
2022-05-30 23:32:15 -0300

    teste first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <b-form-group label="Email">
+      <b-form-input
+        v-model="email"
+        type="email"
+        placeholder="Digite seu e-mail"
+      ></b-form-input>
+    </b-form-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
